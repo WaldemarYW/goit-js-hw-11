e80c04240b053801d6ba131a9f775dc9641fb93a
WaldemarYW <124668448+WaldemarYW@users.noreply.github.com>
2024-04-05 11:20:15 +0300

    homework11
    debug done

--- src/main.js
+++ src/main.js
@@ -11,39 +11,38 @@ const lightbox = new SimpleLightbox('.gallery a', {
   captionDelay: 250,
 });
 
-form.addEventListener('submit', async function (event) {
-  event.preventDefault();
-
-  const query = form.querySelector('input[name="search"]').value.trim();
-  if (!query) {
-    iziToast.error({
-      title: 'Error',
-      message: 'Please enter a search query.',
-      position: 'topCenter',
-    });
-    return;
-  }
-
-  try {
-    const images = await fetchImages(query);
-    if (images.length === 0) {
-      iziToast.warning({
-        title: 'Warning',
-        message:
-          'Sorry, there are no images matching your search query. Please try again.',
-        position: 'topCenter',
-      });
-      return;
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const query = form.querySelector('input[name="search"]').value.trim();
+    if (!query) {
+        iziToast.error({
+            title: 'Error',
+            message: 'Please enter a search query.',
+            position: 'topCenter'
+        });
+        return;
     }
-    renderImages(images);
-    lightbox.refresh();
-  } catch (error) {
-    console.error('Error searching images:', error);
-    iziToast.error({
-      title: 'Error',
-      message:
-        'An error occurred while searching for images. Please try again later.',
-      position: 'topCenter',
-    });
-  }
+
+    fetchImages(query)
+        .then(images => {
+            if (images.length === 0) {
+                iziToast.warning({
+                    title: 'Warning',
+                    message: 'Sorry, there are no images matching your search query. Please try again.',
+                    position: 'topCenter'
+                });
+                return;
+            }
+            renderImages(images);
+            lightbox.refresh();
+        })
+        .catch(error => {
+            console.error('Error searching images:', error);
+            iziToast.error({
+                title: 'Error',
+                message: 'An error occurred while searching for images. Please try again later.',
+                position: 'topCenter'
+            });
+        });
 });
